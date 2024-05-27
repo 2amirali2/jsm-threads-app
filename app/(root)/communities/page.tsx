14a6cb36938ca09799ccc92ad946b96804ca0aeb
aamigo <aamigodeveloper@gmail.com>
2024-05-27 11:21:13 +0330

    first commit

---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,8 @@
+const CummunitiesPage = async () => {
+    return (
+      <section>
+          <h1 className="head-text mb-10">Cummunities</h1>
+      </section>
+    )
+  }
+  export default CummunitiesPage
